d5dc40d9af9977d0ae1256bc0084f174e213167c
Tien Tung Duong <tientungduong@hotmail.com>
2023-05-30 02:02:47 +0200

    create database tables

---
+++ database/migrations/2023_05_29_234134_create_company_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('company', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->string('city');
+            $table->integer('zipcode');
+            $table->string('email');
+            $table->string('password');
+            $table->integer('phone');
+            $table->string('logo');
+            $table->longText('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('company');
+    }
+};
---
+++ database/migrations/2023_05_29_234202_create_user_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->string('city');
+            $table->integer('zipcode');
+            $table->integer('phone');
+            $table->string('gender');
+            $table->string('email');
+            $table->string('password');
+            $table->date('birthdate');
+            $table->string('picture');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user');
+    }
+};
